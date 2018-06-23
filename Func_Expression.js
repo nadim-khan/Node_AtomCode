a5213248f66f4629b9a0d9386ca5a587265f0c59
Nadeem <nadim-khan@users.noreply.github.com>
2018-06-23 15:46:27 +0530

    Create Func_Expression.js
    Working with function expression.

---
+++ Func_Expression.js
@@ -0,0 +1,18 @@
+///Normal function
+function sayHello(){
+  console.log(' Hello friends!! ');
+}
+
+///Calling sayHello function
+sayHello();
+
+
+
+///function Expression
+//Function without name is anonymous Function
+var sayBbye= function(){
+  console.log ('Bbye Friends !!');
+}
+
+/// Calling Function Expression
+sayBbye();
