c368f0d07b8a687f4183b998233177b4c0ba3d4c
Nadeem <nadim-khan@users.noreply.github.com>
2018-06-24 18:56:38 +0530

    Update Counter.js

--- Modules and require()/Counter.js
+++ Modules and require()/Counter.js
@@ -8,4 +8,17 @@ console.log(counter(['Nadeem','Rahul','Vivek']));
 
 //console.log(counter(array));
 
-module.exports=counter;     //the part which you want to made available for require()
+//module.exports=counter;     //the part which you want to made available for require()
+
+
+
+var addit= function(a,b){
+  return 'addition of the two numbers $a and $b are ${a+b}';
+}
+
+var pi =3.142
+
+
+module.exports.counter=counter;     //the part which you want to made available for require()
+module.exports.addit=addit;
+module.exports.pi=pi;
