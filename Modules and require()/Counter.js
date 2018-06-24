a952677e7df863a13525836e2461c1517c8a793a
Nadeem <nadim-khan@users.noreply.github.com>
2018-06-24 19:17:36 +0530

    Update Counter.js
    Module patterns, multiple objects which needs to be reused at different files

--- Modules and require()/Counter.js
+++ Modules and require()/Counter.js
@@ -1,9 +1,9 @@
 ///passing array as argument to the function
-var array= ['Nadeem','Rahul','Vivek','Vijay'];
-var counter=function (array){
-  console.log('There are '+array.length+' elementes in Array in 1st file');
-}
-console.log(counter(['Nadeem','Rahul','Vivek']));
+//var array= ['Nadeem','Rahul','Vivek','Vijay'];
+module.exports.counter = function (array){
+  return 'There are '+array.length+' elementes in Array ';
+};
+//console.log(counter(['Nadeem','Rahul','Vivek']));
 /// 2nd method
 
 //console.log(counter(array));
@@ -12,13 +12,14 @@ console.log(counter(['Nadeem','Rahul','Vivek']));
 
 
 
-var addit= function(a,b){
-  return 'addition of the two numbers $a and $b are ${a+b}';
-}
+module.exports.addit = function(a,b){
+  return `addition of the two numbers  are ${a+b }`;
+};
 
-var pi =3.142
+module.exports.pi = 3.142;
 
-
-module.exports.counter=counter;     //the part which you want to made available for require()
-module.exports.addit=addit;
-module.exports.pi=pi;
+/*
+=counter;     //the part which you want to made available for require()
+=addit;
+=pi;
+*/
