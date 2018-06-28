af0f40401c12ca4dd30cf0be7e28cfde090b5c6e
Nadeem <nadim-khan@users.noreply.github.com>
2018-06-28 20:53:51 +0530

    Update #5-Streams_Buffers.js

--- #5-Streams_Buffers.js
+++ #5-Streams_Buffers.js
@@ -2,7 +2,14 @@ var http=require('http');
 var fs=require('fs');//Module for Files
 
 var myRead=fs.createReadStream(__dirname+'/readMe.txt','utf8');//to read file
+
+var myWrite=fs.createWriteStream(__dirname+'/writeMe.txt');
+
 myRead.on('data',function(chunk){     // Data - will receive first chunk of data and pass it on...
   console.log('New Chunk Received : ');
   console.log(chunk);
+  console.log('Storing data to writeMe.txt file');
+  myWrite.write(chunk);
+
 });
+//unlink(writeMe.txt);
