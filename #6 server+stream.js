df9e7ebfac9ca308f1695a3e0a54cbab415f9bb6
Nadeem <nadim-khan@users.noreply.github.com>
2018-07-02 00:36:53 +0530

    Create #6 server+stream.js
    creating a server , Reading a file from the stream and sending as the response

---
+++ #6 server+stream.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var fs=require('fs');//Module for Files
+
+var server=http.createServer(function(req,res){
+    console.log('Request made for :'+req.url);
+    res.writeHead(200,{'Content-Type':'Text/Plain'});    
+    var myRead=fs.createReadStream(__dirname+'/readMe.txt','utf8');//to read file
+    //var myWrite=fs.createWriteStream(__dirname+'/writeMe.txt');
+    myRead.pipe(res);//Sending read files as the response..
+    //console.log(x);
+    //res.end('Ending Response cycle For server');
+});
+server.listen(3000,'127.0.0.1');
+console.log('Server created as localhost ');
+console.log('Now Listening to port 3000 ');
+//console.log('Request made for :'+req.url);
+
+
