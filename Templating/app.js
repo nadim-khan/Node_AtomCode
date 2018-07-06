c6e19604c640bc6e05c414af5b92fc2dd3665a24
Nadeem <nadim-khan@users.noreply.github.com>
2018-07-06 17:10:59 +0530

    Update app.js

--- Templating/app.js
+++ Templating/app.js
@@ -1,8 +1,8 @@
-var express=require('express');//express module returns a function
-var app=express();
-app.set('view engine','ejs');
+var express=require('express');//express module returns a function 
 //which is stord by app
+var app=express();
 
+app.set('view engine','ejs');/// setting a view as ejs
 
 app.get('/',function(req,res){
     //res.send('This is the Home page');
@@ -15,7 +15,11 @@ app.get('/contact',function(req,res){
     console.log('User requested for Contact page');
 });
 app.get('/profile/:id',function(req,res){
-    res.render('profile',{person: req.params.id});
-    console.log('User Requested for ID : '+req.params.id+' inside profile page ');
+    var data={ age: 25, job:'Software Engineer',hobby:['Coding','Playing','Sleeping']};
+    //res.send('You have requested for Id : '+req.params.id);
+    //console.log('User Requested for ID : '+req.params.id+' inside profile page ');
+    res.render('profile', {person: req.params.id, data:data});
+    console.log('User requested for '+req.url+' page');
 });
 app.listen(3000);
+
